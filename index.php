66604e0ffb04459dfe668929f055aef1aaacb386
Thuan Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-05 14:28:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require_once('header.php');?>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Data Mining</title>
+    <title>Character Records</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
@@ -53,7 +53,7 @@ $conn->close();
 ?>
   </tbody>
     </table>
-    <h1>Commencing data mining!</h1>
+    <h1>Time to go on an adventure! Huzzah!</h1>
    
     
     
