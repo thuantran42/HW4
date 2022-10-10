9bdbf38128968968e7ff5fd4a148d2969f09e611
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-10 14:37:42 -0500

    Update rpgquest.php

--- rpgquest.php
+++ rpgquest.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Courses</h1>
+    <h1>Quests</h1>
 <table class="table table-striped">
   <thead>
     <tr>
