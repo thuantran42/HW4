f36b4f7f502428674ee8a4bada72f5d41ede4224
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-10 14:20:54 -0500

    Update and rename course-section.php to characterquest.php

--- characterquest.php
+++ characterquest.php
@@ -3,19 +3,19 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sections</title>
+    <title>Quest Acceptance</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Sections</h1>
+    <h1>Quest Acceptance</h1>
 <table class="table table-striped">
   <thead>
     <tr>
-      <th>ID</th>
-      <th>Prefix</th>
-      <th>Number</th>
-      <th>Section</th>
-      <th>Instructor</th>
+      <th>Quest Accept ID</th>
+      <th>Offer</th>
+      <th>Quest Name</th>
+      <th>Character Class</th>
+      <th>Character</th>
     </tr>
   </thead>
   <tbody>
@@ -23,7 +23,7 @@
 $servername = "localhost";
 $username = "projecto_homework3";
 $password = "0w_zeP}]OVy0";
-$dbname = "projecto_homework3";
+$dbname = "projecto_homework4";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -33,7 +33,13 @@ if ($conn->connect_error) {
 }
 $cid = $_POST['id'];
 //echo $iid;
-$sql = "select section_id, section_number, i.instructor_name, c.prefix, c.number from section s join instructor i on i.instructor_id = s.instructor_id join course c on c.course_id = s.course_id where c.course_id=" . $cid;
+$sql = "select cqAcceptance_id, cqOffer, questName, char.rpgClass, char.rpgName 
+from RPGCharacterQuest cq
+join RPGCharacter char 
+on char.rpg_id = cq.rpg_id 
+join RPGQuest que
+on que.quest_id = cq.quest_id 
+where que.course_id=" . $cid;
 //echo $sql;
     $result = $conn->query($sql);
 
@@ -42,11 +48,11 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
 ?>
   <tr>
-    <td><?=$row["section_id"]?></td>
-    <td><?=$row["prefix"]?></td>
-    <td><?=$row["number"]?></td>
-    <td><?=$row["section_number"]?></td>
-    <td><?=$row["instructor_name"]?></td>
+    <td><?=$row["cqAcceptance_id"]?></td>
+    <td><?=$row["cqOffer"]?></td>
+    <td><?=$row["questName"]?></td>
+    <td><?=$row["rpgClass"]?></td>
+    <td><?=$row["rpgName"]?></td>
   </tr>
 <?php
   }
