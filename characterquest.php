8c4ae6bd985586443ae74741cb72ee3a149ed197
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-12 13:40:33 -0500

    Update characterquest.php

--- characterquest.php
+++ characterquest.php
@@ -31,7 +31,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$cid = $_POST['quest_id'];
+$cid = $_POST['id'];
 //echo $iid;
 $sql = "select cqAcceptance_id, cqOffer, questName, char.rpgClass, char.rpgName 
 from RPGCharacterQuest cq
