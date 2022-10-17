1166c8e49f8de70cd75d0a254f126e8ccb73fe86
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-17 12:55:47 -0500

    Update characterquest.php

--- characterquest.php
+++ characterquest.php
@@ -3,17 +3,17 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sections</title>
+    <title>Character Quests</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Sections</h1>
+    <h1>Character Quests</h1>
 <table class="table table-striped">
   <thead>
     <tr>
       <th>Acceptance ID</th>
       <th>Offer</th>
-      <thCharacter</th>
+      <th>Character</th>
       <th>Quest Description</th>
       <th>Quest Name</th>
     </tr>
