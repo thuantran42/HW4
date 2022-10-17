4868586bc2cd5ba8de8a9e753b36ef300795c88d
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-17 12:39:57 -0500

    Update character-cards.php

--- character-cards.php
+++ character-cards.php
@@ -44,7 +44,7 @@ if ($result->num_rows > 0) {
     $section_result = $conn->query($section_sql);
     
     while($section_row = $section_result->fetch_assoc()) {
-      echo "<li>" . $section_row["description"] . "</li>";
+      echo "<li>" . $section_row["questDesc"] . "</li>";
     }
 ?>
       </ul></p>
