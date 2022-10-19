36d4421450c579d5ee5a411bf8e3d946bd52d96b
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-19 14:08:52 -0500

    Update rpgcharacter.php

--- rpgcharacter.php
+++ rpgcharacter.php
@@ -53,7 +53,7 @@ $conn->close();
 ?>
   </tbody>
     </table>
-    <a class="btn btn-primary" href="character-add.php" role="button">Back to Home Page</a>
+    <a class="btn btn-primary" href="character-add.php" role="button">Add a character</a>
    <a class="btn btn-primary" href="index.php" role="button">Back to Home Page</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
