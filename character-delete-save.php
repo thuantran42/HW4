38bfb6e174987f72df64d92af314e89b98747af0
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-21 13:38:52 -0500

    Update character-delete-save.php

--- character-delete-save.php
+++ character-delete-save.php
@@ -31,7 +31,7 @@ $sql = "delete from RPGCharacter where rpg_id=?";
     
     <h1>Delete Character</h1>
 <div class="alert alert-success" role="alert">
-  Instructor deleted.
+  Character deleted.
 </div>
     <a href="rpgcharacter.php" class="btn btn-primary">Go back</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
