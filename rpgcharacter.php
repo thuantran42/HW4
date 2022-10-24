8062e222cdd6c6468b0328b195d224e7ebb7fc0e
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-24 02:56:50 -0500

    Update rpgcharacter.php

--- rpgcharacter.php
+++ rpgcharacter.php
@@ -30,20 +30,93 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "SELECT rpg_id, rpgName, rpgElement, rpgClass from RPGCharacter";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  switch ($_POST['saveType']) {
+    case 'Add':
+      $sqlAdd = "insert into RPGCharacter (rpgName) value (?)";
+      $stmtAdd = $conn->prepare($sqlAdd);
+      $stmtAdd->bind_param("s", $_POST['iName']);
+      $stmtAdd->execute();
+      echo '<div class="alert alert-success" role="alert">New character added.</div>';
+      break;
+    case 'Edit':
+      $sqlEdit = "update RPGCharacter set rpgName=? where rpg_id=?";
+      $stmtEdit = $conn->prepare($sqlEdit);
+      $stmtEdit->bind_param("si", $_POST['iName'], $_POST['iid']);
+      $stmtEdit->execute();
+      echo '<div class="alert alert-success" role="alert">Character edited.</div>';
+      break;
+    case 'Delete':
+      $sqlDelete = "delete from RPGCharacter where rpg_id=?";
+      $stmtDelete = $conn->prepare($sqlDelete);
+      $stmtDelete->bind_param("i", $_POST['iid']);
+      $stmtDelete->execute();
+      echo '<div class="alert alert-success" role="alert">Character deleted.</div>';
+      break;
+  }
+}
+?>
+    
+      <h1>Characters</h1>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          
+<?php
+$sql = "SELECT rpg_id, rpgName from RPGCharacter";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-  <tr>
-    <td><?=$row["rpg_id"]?></td>
-    <td><a href="character-characterquest.php?id=<?=$row["rpg_id"]?>"><?=$row["rpgName"]?></a></td>
-    <td><?=$row["rpgElement"]?></td>
-    <td><?=$row["rpgClass"]?></td>
-  </tr>
+          
+          <tr>
+            <td><?=$row["rpg_id"]?></td>
+            <td><a href="character-characterquest.php?id=<?=$row["rpg_id"]?>"><?=$row["rpgName"]?></a></td>
+            <td>
+              <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editCharacter<?=$row["rpg_id"]?>">
+                Edit
+              </button>
+              <div class="modal fade" id="editCharacter<?=$row["rpg_id"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editCharacter<?=$row["rpg_id"]?>Label" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="editCharacter<?=$row["rpg_id"]?>Label">Edit Character</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form method="post" action="">
+                        <div class="mb-3">
+                          <label for="editCharacter<?=$row["rpg_id"]?>Name" class="form-label">Name</label>
+                          <input type="text" class="form-control" id="editCharacter<?=$row["rpg_id"]?>Name" aria-describedby="editCharacter<?=$row["rpg_id"]?>Help" name="iName" value="<?=$row['rpgName']?>">
+                          <div id="editCharacter<?=$row["rpg_id"]?>Help" class="form-text">Enter the character's name.</div>
+                        </div>
+                        <input type="hidden" name="iid" value="<?=$row['rpg_id']?>">
+                        <input type="hidden" name="saveType" value="Edit">
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <form method="post" action="">
+                <input type="hidden" name="iid" value="<?=$row["rpg_id"]?>" />
+                <input type="hidden" name="saveType" value="Delete">
+                <input type="submit" class="btn" onclick="return confirm('Are you sure?')" value="Delete">
+              </form>
+            </td>
+          </tr>
+          
 <?php
   }
 } else {
@@ -51,11 +124,38 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-  </tbody>
-    </table>
-    <a class="btn btn-primary" href="character-add.php" role="button">Add a character</a>
-    <a class="btn btn-primary" href="character-delete-save.php" role="button">Delete a character</a>
-   <a class="btn btn-primary" href="index.php" role="button">Back to Home Page</a>
+          
+        </tbody>
+      </table>
+      <br />
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCharacter">
+        Add New
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="addCharacter" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addCharacterLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="addCharacterLabel">Add Character</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="CharacterName" class="form-label">Name</label>
+                  <input type="text" class="form-control" id="CharacterName" aria-describedby="nameHelp" name="iName">
+                  <div id="nameHelp" class="form-text">Enter the Character's name.</div>
+                </div>
+                <input type="hidden" name="saveType" value="Add">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
