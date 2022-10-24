ded46d2bd75024145b5a1ea2c90e93e7d86fd383
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-24 03:23:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ $conn->close();
   </tbody>
     </table>
     <h1>Time to go on an adventure! Huzzah!</h1>
-   
+   <img src="olafSnowman.jfif" alt="Huzzah Man" style="height:100px;" />
     
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
