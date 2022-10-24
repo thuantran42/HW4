f10ce47a5e53f91eb552b2907d14731f1f14ced2
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-24 03:11:58 -0500

    Update rpgcharacter.php

--- rpgcharacter.php
+++ rpgcharacter.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>RPG Characters</h1>
+
 
     <?php
 $servername = "localhost";
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
     
-      <h1>Characters</h1>
+      <h1> RPG Characters</h1>
       <table class="table table-striped">
         <thead>
           <tr>
