ac7f4cb9d1c9291528d2c8549f081f7dd7f98d48
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-25 13:53:54 -0500

    Update characterquest-edit-save.php

--- characterquest-edit-save.php
+++ characterquest-edit-save.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Edit Character</title>
+    <title>Editing Character's Quest Offer</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
@@ -29,9 +29,9 @@ $sql = "update RPGCharacterQuest set cqOffer=? where rpg_id=?";
     $stmt->execute();
 ?>
     
-    <h1>Edit Character</h1>
+    <h1>Edit Quest Offer</h1>
 <div class="alert alert-success" role="alert">
-  Character edited.
+  Offer Amount edited.
 </div>
     <a href="rpgcharacter.php" class="btn btn-primary">Back to Character Page</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
