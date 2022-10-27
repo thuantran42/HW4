cc265e8e4da98a2f6d57264aee494234ba850d4f
Thuan (Trae) Tran <70498283+thuantran42@users.noreply.github.com>
2022-10-27 00:26:54 -0500

    Update shoes.php

--- shoes.php
+++ shoes.php
@@ -24,21 +24,21 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
     switch ($_POST['saveType']) {
     case 'Add':
-    $sqlAdd = "insert into RPGClothes (shoe_name) value (?)";
+    $sqlAdd = "insert into RPGShoe (shoe_name) value (?)";
     $stmtAdd = $conn->prepare($sqlAdd);
     $stmtAdd->bind_param("s", $_POST['iName']);
     $stmtAdd->execute();
     echo '<div class="alert alert-success" role="alert">New shoe added.</div>';
     break;
     case 'Edit':
-    $sqlEdit = "update RPGClothes set shoe_name=? where shoe_id=?";
+    $sqlEdit = "update RPGShoe set shoe_name=? where shoe_id=?";
     $stmtEdit = $conn->prepare($sqlEdit);
     $stmtEdit->bind_param("si", $_POST['iName'], $_POST['iid']);
     $stmtEdit->execute();
     echo '<div class="alert alert-success" role="alert">shoe edited.</div>';
     break;
     case 'Delete':
-    $sqlDelete = "delete from RPGClothes where shoe_id=?";
+    $sqlDelete = "delete from RPGShoe where shoe_id=?";
     $stmtDelete = $conn->prepare($sqlDelete);
     $stmtDelete->bind_param("i", $_POST['iid']);
     $stmtDelete->execute();
@@ -73,24 +73,24 @@
                 <td><?=$row["shoe_id"]?></td>
                 <td><?=$row["shoe_name"]?></td>
                 <td>
-                    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editClothes<?=$row[" shoe_id"]?>
+                    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editShoes<?=$row[" shoe_id"]?>
                         ">
                         Edit
                     </button>
-                    <div class="modal fade" id="editClothes<?=$row[" shoe_id"]?>
-                        " data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editClothes<?=$row["shoe_id"]?>Label" aria-hidden="true">
+                    <div class="modal fade" id="editShoes<?=$row[" shoe_id"]?>
+                        " data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editShoes<?=$row["shoe_id"]?>Label" aria-hidden="true">
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="editClothes<?=$row[" shoe_id"]?>Label">Edit shoe</h1>
+                                    <h1 class="modal-title fs-5" id="editShoe<?=$row[" shoe_id"]?>Label">Edit shoe</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
                                     <form method="post" action="">
                                         <div class="mb-3">
-                                            <label for="editClothes<?=$row[" shoe_id"]?>Name" class="form-label">Name</label>
-                                            <input type="text" class="form-control" id="editClothes<?=$row[" shoe_id"]?>Name" aria-describedby="editClothes<?=$row["shoe_id"]?>Help" name="iName" value="<?=$row['shoe_name']?>">
-                                            <div id="editClothes<?=$row[" shoe_id"]?>Help" class="form-text">Enter the shoe's name.</div>
+                                            <label for="editShoe<?=$row[" shoe_id"]?>Name" class="form-label">Name</label>
+                                            <input type="text" class="form-control" id="editShoe<?=$row[" shoe_id"]?>Name" aria-describedby="editShoe<?=$row["shoe_id"]?>Help" name="iName" value="<?=$row['shoe_name']?>">
+                                            <div id="editShoe<?=$row[" shoe_id"]?>Help" class="form-text">Enter the shoe's name.</div>
                                         </div>
                                         <input type="hidden" name="iid" value="<?=$row['shoe_id']?>">
                                         <input type="hidden" name="saveType" value="Edit">
